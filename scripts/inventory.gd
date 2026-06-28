1c5acddf0132cad4006c03d6d284c0fd7f3aab79
CrypticSafe5 <WesleyBrackett94@gmail.com>
2026-06-27 19:31:13 -0500

    fixed npe on item addition and added handling for count

--- scripts/inventory.gd
+++ scripts/inventory.gd
@@ -7,34 +7,43 @@ class InventoryItem:
 var storage: Array[InventoryItem] = []
 @export var count_capacity: int = -1
 
-func add(item: Item) -> void:
+func add(item: Item, count: int) -> void:
 	"""
 	Add an item to the inventory and returns it's positional index.
+
+	TODO:
+	- Add position to target addition of item
 	"""
 	var index: int
-	#var inv_ids = storage.keys()
 	var inv_count = len(storage)
 	for inv_index in range(inv_count):
 		var inv_item = storage[inv_index]
-		if inv_item.id == item.id and inv_item.count < item.stack_size:
+		var is_item = inv_item.id == item.id
+		if is_item and inv_item.count < item.stack_size:
 			# Matching item with stack space
-			storage[inv_index].count += 1
-			index = inv_index
-			break
+			var stack_count = inv_item.count + count
+			if stack_count > item.stack_size:
+				# Stack overflow
+				var diff = stack_count - item.stack_size
+				storage[inv_index].count = item.stack_size
+				add(item, diff)
+			else:
+				# Sub stack addition
+				storage[inv_index].count += stack_count
+			return
 		elif inv_item == null:
 			# Empty slot open
 			index = inv_index
-			storage[inv_index].count += 1
 			break
 
 	# Fallback by appending if space is left
-	if index == null and inv_count < count_capacity:
-		index = inv_count
-		storage.append({
-			"item": item,
-			"count": 1
-		})
+	var should_append = index == null and inv_count < count_capacity
+	index = inv_count if should_append else index
 	assert(count_capacity < index, "Inventory full, unable to add {}".format([item.name], "{}"))
+	storage.append({
+		"item": item,
+		"count": count
+	})
 
 func remove(index: int, count: int) -> void:
 	"""
